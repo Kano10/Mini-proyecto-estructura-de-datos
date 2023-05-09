ca80e45563873e949c7221951e02fc9b8f2de037
Kano10 <canomga@gmail.com>
2023-05-08 22:21:10 -0400

    probando en main

--- main.cpp
+++ main.cpp
@@ -7,7 +7,12 @@
 using namespace std;
 
 int main(){
-
+        ListArr* array = new ListArr(10, 5);
+    cout<<"LISTARR CREADO"<<endl;
+    //for(int i = 0; i < 10; ++i){
+        array->insert(1, 0);
+    //}
+    array->print(array->getFirst());
 
 
     return 0;
